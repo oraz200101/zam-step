055a35535d19a5c7e9d271e068842bf3d023439f
oziyabek <oraz20010101@gmail.com>
2024-05-20 22:07:00 +0500

    автоматическое поднятие через докер

--- api-gateway/src/main/java/com/example/apigateway/config/RouteLocatorConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/config/RouteLocatorConfig.java
@@ -1,5 +1,6 @@
 package com.example.apigateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -7,13 +8,35 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RouteLocatorConfig {
-    // todo Oraz route нужно перенести в конфиг
+
+    @Value("${api.routes.auth}")
+    private String authURI;
+
+    @Value("${api.routes.user-service}")
+    private String userServiceURI;
+
+    @Value("${api.routes.health-service}")
+    private String healthServiceURI;
+
+    @Value("${api.routes.cryptocurrency-transaction-service}")
+    private String cryptocurrencyTransactionServiceURI;
+
+    @Value("${api.routes.cryptocurrency-wallet-service}")
+    private String cryptocurrencyWalletServiceURI;
+
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
-                                  .route("auth-route", r -> r.path("/api/auth/**").uri("http://localhost:8081/api/auth"))
-                                  .route("user-service-route", r -> r.path("/api/user/**").uri("http://localhost:8082/api/user"))
-                                  .route("health-serviced-route", r -> r.path("/api/health-analysis/**").uri("http://localhost:8084/api/health-analysis"))
+                                  .route("auth-route",
+                                         r -> r.path("/api/auth/**").uri(authURI))
+                                  .route("user-service-route",
+                                         r -> r.path("/api/user/**").uri(userServiceURI))
+                                  .route("health-service-route",
+                                         r -> r.path("/api/health-analysis/**").uri(healthServiceURI))
+                                  .route("cryptocurrency-service-wallet-route",
+                                         r -> r.path("/api/wallets/**").uri(cryptocurrencyWalletServiceURI))
+                                  .route("cryptocurrency-service-transaction-route",
+                                         r -> r.path("/api/transactions/**").uri(cryptocurrencyTransactionServiceURI))
                                   .build();
     }
 }
