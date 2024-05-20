3f47e38ed711b47966b8b93f10b234051a3b84c1
oziyabek <oraz20010101@gmail.com>
2024-05-21 00:17:55 +0500

    убрал bigchain db

--- health-service/src/main/java/com/example/healthservice/config/BigchainDbConfig.java
+++ health-service/src/main/java/com/example/healthservice/config/BigchainDbConfig.java
@@ -5,6 +5,7 @@ import com.bigchaindb.constants.BigchainDbApi;
 import com.bigchaindb.model.BigChainDBGlobals;
 import jakarta.annotation.PostConstruct;
 import net.i2p.crypto.eddsa.KeyPairGenerator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,13 +14,14 @@ import java.security.KeyPair;
 @Configuration
 public class BigchainDbConfig {
 
-    private static final String BIGCHAINDB_URL = "http://localhost:9984";
+    @Value("${bigchaindb.URI}")
+    private static String bigchain_URI;
 
     @PostConstruct
     public static void setupConnection() {
         // Установка конфигурации BigchainDB
         BigchainDbConfigBuilder
-                .baseUrl(BIGCHAINDB_URL) // URL-адрес узла BigchainDB
+                .baseUrl(bigchain_URI) // URL-адрес узла BigchainDB
                 .setup(); // Установка конфигурации
 
         // Установка API-адреса BigchainDB (необязательно, но может быть полезно для настройки пользовательского API)
